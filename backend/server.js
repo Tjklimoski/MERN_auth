030dceac8f03cc51c0579a1de1b5079c85e6e472
TJ Klimoski <tjklimoski@gmail.com>
2023-07-05 13:30:34 -0400

    Connect error handler functions as middleware to server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -12,5 +13,9 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("Home route");
 });
+
+// Error handler middleware must be placed at bottom of server to catch all errors
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
