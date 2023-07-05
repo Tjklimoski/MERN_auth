06827c71f3d1f1cc843c90154459e6d506823199
TJ Klimoski <tjklimoski@gmail.com>
2023-07-05 12:51:07 -0400

    Turn controller function async

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,8 @@
 // desc    Auth user and set token
 // route   POST /api/users/auth
 // access  Public
-function authUser(req, res) {
+const authUser = async (req, res) => {
   res.status(200).json({ message: "Auth User" });
-}
+};
 
 export { authUser };
