a339e847cf19d64937ea0d8d3276701fe0b94699
TJ Klimoski <tjklimoski@gmail.com>
2023-07-05 13:24:46 -0400

    Create custom errorHandler for express

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -5,3 +5,21 @@ const notFound = (req, res, next) => {
   res.status(404);
   next(error);
 };
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  //To change the error status and message on mongoose errors (to be more user friendly)
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
