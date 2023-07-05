29250029b25e333171276fbfea7809e44afc7d63
TJ Klimoski <tjklimoski@gmail.com>
2023-07-05 16:06:07 -0400

    Setup express to handle form submission and json data in body of requests

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
