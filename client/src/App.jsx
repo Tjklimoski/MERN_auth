463ebac3f049a5e88a85fe22cd2afff271a271f3
TJ Klimoski <tjklimoski@gmail.com>
2023-07-06 16:18:45 -0400

    Setup react router and render index route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
-      <HomePage />
+      <Outlet />
     </>
   );
 }
